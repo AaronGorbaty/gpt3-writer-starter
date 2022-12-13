8951026b9f09969187c25b9871a2e7671f387acd
Aaron Gorbaty <aarongorbaty@wireless-10-105-18-160.umd.edu>
2022-12-12 21:10:31 -0500

    first commit

---
+++ pages/api/generate.js
@@ -0,0 +1,25 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = "This is a Q&A session with an orthodox Rabbi who is very learned and a deep thinking, knows everything about Judaism. Question: ";
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.5,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,91 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
+// import logo from './assets/logoATR.png';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+
+  const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+  
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+  
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
   return (
+    
     <div className="root">
+
+    
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>AsktheRabbiNew</title>
       </Head>
+      
+
+
       <div className="container">
         <div className="header">
+          
+        {/* <div>
+          <img src={logo} width="300" height="300"  alt="logoATR" />
+        </div> */}
+
+          
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Ask the Rabbi</h1>
           </div>
-          <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
-          </div>
+        
         </div>
+        <div className="prompt-container">
+          <textarea placeholder="Enter what you'd like to ask the Rabbi" 
+          className="prompt-box" 
+          value={userInput} 
+          onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+   
+    </div>
+    {apiOutput && (
+  <div className="output">
+    <div className="output-header-container">
+      <div className="output-header">
+        <h3>Output</h3>
       </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
+    </div>
+    <div className="output-content">
+      <p>{apiOutput}</p>
+    </div>
+  </div>
+)}
+<div className="prompt-buttons">
+  <a
+    className={isGenerating ? 'generate-button loading' : 'generate-button'}
+    onClick={callGenerateEndpoint}
+  >
+    <div className="generate">
+    {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+    </div>
+  </a>
+</div>
+
+        </div>
       </div>
     </div>
   );
